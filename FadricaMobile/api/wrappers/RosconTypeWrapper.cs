5b8fa60acb43c4e374d38a1da4be132aa38cdfeb
Jorge <jorge.gutierrezvila0@gmail.com>
2020-12-29 23:38:45 +0100

    Wrapper made

---
+++ FadricaMobile/api/wrappers/RosconTypeWrapper.cs
@@ -0,0 +1,37 @@
+﻿using FadricaMobile.api.models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FadricaMobile.api.wrappers
+{
+    class RosconTypeWrapper
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private readonly string tipoRosconUrl = "api/tipo-roscon/";
+
+        public RosconTypeWrapper()
+        {
+            client.BaseAddress = new Uri(Constants.BaseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<RosconType>> GetAllRosconTypeAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync($"{tipoRosconUrl}?token={Constants.Token}");
+            List<RosconType> rosconTypes = null;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var resp = await response.Content.ReadAsStringAsync();
+                rosconTypes = JsonConvert.DeserializeObject<List<RosconType>>(resp);
+            }
+
+            return rosconTypes;
+        }
+    }
+}
---
+++ FadricaMobile/api/wrappers/RosconWrapper.cs
@@ -0,0 +1,63 @@
+﻿using FadricaMobile.api.models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FadricaMobile.api.wrappers
+{
+    class RosconWrapper
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private readonly string rosconUrl = "api/roscones/";
+
+        public RosconWrapper()
+        {
+            client.BaseAddress = new Uri(Constants.BaseUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<List<Roscon>> GetAllRosconesAsync(int? year)
+        {
+            if (!year.HasValue)
+                year = DateTime.Now.Year;
+            HttpResponseMessage response = await client.GetAsync($"{rosconUrl}?token={Constants.Token}&year={year}");
+            List<Roscon> roscones = null;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var resp = await response.Content.ReadAsStringAsync();
+                roscones = JsonConvert.DeserializeObject<List<Roscon>>(resp);
+            }
+
+            return roscones;
+        }
+
+        public async Task<bool> CreateRoscon(Roscon roscon)
+        {
+            string json = JsonConvert.SerializeObject(roscon, Formatting.Indented);
+            var content = new StringContent(json.ToLower(), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync($"{rosconUrl}?token={Constants.Token}", content);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<Roscon> UpdateRoscon(Roscon roscon)
+        {
+            string resp;
+            string json = JsonConvert.SerializeObject(roscon, Formatting.Indented);
+            var content = new StringContent(json.ToLower(), Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PutAsync($"{rosconUrl}{roscon.Id}/?token={Constants.Token}", content);
+
+            response.EnsureSuccessStatusCode();
+
+            resp = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<Roscon>(resp);
+        }
+    }
+}
