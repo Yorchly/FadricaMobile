e7c15091e722cb1e88e4e08d964829659dfdff5b
Jorge <jorge.gutierrezvila0@gmail.com>
2020-12-30 17:02:06 +0100

    Basic structure v1

---
+++ FadricaMobile/MainPage.cs
@@ -0,0 +1,104 @@
+﻿using FadricaMobile.api.models;
+using FadricaMobile.api.wrappers;
+using System;
+using System.Collections.Generic;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FadricaMobile
+{
+    class MainPage : ContentPage
+    {
+        private readonly RosconWrapper rosconWrapper = new RosconWrapper();
+        private readonly RosconTypeWrapper rosconTypeWrapper = new RosconTypeWrapper();
+        private readonly List<Roscon> roscones;
+        private readonly List<RosconType> rosconTypes;
+        private readonly Label totalLabel = new Label() { Text = "0" };
+
+        public MainPage()
+        {
+            StackLayout panel = new StackLayout() { Spacing = 15 };
+            Frame titleFrame = new Frame()
+            {
+                BackgroundColor = Color.FromHex("#2196F3"),
+                Padding = new Thickness(17, 17, 17, 17),
+                CornerRadius = 0,
+                Content = new Label()
+                {
+                    Text = "Lista de Roscones",
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    TextColor = Color.White,
+                    FontSize = 36
+                }
+            };
+
+            if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                roscones = rosconWrapper.GetAllRosconesAsync(null).GetAwaiter().GetResult();
+                rosconTypes = rosconTypeWrapper.GetAllRosconTypeAsync().GetAwaiter().GetResult();
+
+                Button saveButton = new Button()
+                {
+                    Text = "Guardar"
+                };
+                Button updateButton = new Button()
+                {
+                    Text = "Actualizar"
+                };
+
+                saveButton.Clicked += SaveEvent;
+                updateButton.Clicked += UpdateEvent;
+
+                panel.Children.Add(titleFrame);
+                SetRosconesAndTypes(roscones, rosconTypes, panel);
+                panel.Children.Add(totalLabel);
+                panel.Children.Add(saveButton);
+                panel.Children.Add(updateButton);
+            }
+            else
+            {
+                panel.Children.Add(new Label { 
+                    Text = "No tienes conexión a internet"
+                });
+            }
+            this.Content = panel;
+        }
+
+        private void SetRosconesAndTypes(List<Roscon> roscones, List<RosconType> rosconTypes, StackLayout panel)
+        {
+            foreach (var rosconType in rosconTypes)
+            {
+                Label rosconTypeLabel = new Label()
+                {
+                    Text = rosconType.Tipo
+                };
+                Roscon roscon = roscones.Find(x => x.Tipo_Roscon == rosconType.Id);
+                Entry rosconInput = new Entry()
+                {
+                    Text = roscon.Cantidad.ToString(),
+                    Keyboard = Keyboard.Numeric
+                };
+
+                rosconInput.Completed += TotalAmountEvent;
+
+                panel.Children.Add(rosconTypeLabel);
+                panel.Children.Add(rosconInput);
+            }
+        }
+
+        private void SaveEvent (object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateEvent (object sender, EventArgs e)
+        {
+
+        }
+
+        private void TotalAmountEvent (object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- FadricaMobile/api/wrappers/RosconWrapper.cs
+++ FadricaMobile/api/wrappers/RosconWrapper.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms.Internals;
 
 namespace FadricaMobile.api.wrappers
 {
@@ -23,41 +24,66 @@ namespace FadricaMobile.api.wrappers
 
         public async Task<List<Roscon>> GetAllRosconesAsync(int? year)
         {
-            if (!year.HasValue)
-                year = DateTime.Now.Year;
-            HttpResponseMessage response = await client.GetAsync($"{rosconUrl}?token={Constants.Token}&year={year}");
-            List<Roscon> roscones = null;
-
-            if (response.IsSuccessStatusCode)
+            try
             {
-                var resp = await response.Content.ReadAsStringAsync();
-                roscones = JsonConvert.DeserializeObject<List<Roscon>>(resp);
+                if (!year.HasValue)
+                    year = DateTime.Now.Year;
+                HttpResponseMessage response = await client.GetAsync($"{rosconUrl}?token={Constants.Token}&year={year}");
+                List<Roscon> roscones = null;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var resp = await response.Content.ReadAsStringAsync();
+                    roscones = JsonConvert.DeserializeObject<List<Roscon>>(resp);
+                }
+
+                return roscones;
             }
-
-            return roscones;
+            catch (Exception e)
+            {
+                Log.Warning("GetAllRosconesAsync error", $"Error -> {e.Message}");
+                throw e;
+            }
+            
         }
 
-        public async Task<bool> CreateRoscon(Roscon roscon)
+        public async Task<bool> CreateRosconAsync(Roscon roscon)
         {
-            string json = JsonConvert.SerializeObject(roscon, Formatting.Indented);
-            var content = new StringContent(json.ToLower(), Encoding.UTF8, "application/json");
-            HttpResponseMessage response = await client.PostAsync($"{rosconUrl}?token={Constants.Token}", content);
+            try
+            {
+                string json = JsonConvert.SerializeObject(roscon, Formatting.Indented);
+                var content = new StringContent(json.ToLower(), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync($"{rosconUrl}?token={Constants.Token}", content);
 
-            return response.IsSuccessStatusCode;
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                Log.Warning("CreateRosconAsync error", $"Error -> {e.Message}");
+                throw e;
+            }
         }
 
-        public async Task<Roscon> UpdateRoscon(Roscon roscon)
+        public async Task<Roscon> UpdateRosconAsync(Roscon roscon)
         {
-            string resp;
-            string json = JsonConvert.SerializeObject(roscon, Formatting.Indented);
-            var content = new StringContent(json.ToLower(), Encoding.UTF8, "application/json");
-            HttpResponseMessage response = await client.PutAsync($"{rosconUrl}{roscon.Id}/?token={Constants.Token}", content);
+            try
+            {
+                string resp;
+                string json = JsonConvert.SerializeObject(roscon, Formatting.Indented);
+                var content = new StringContent(json.ToLower(), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PutAsync($"{rosconUrl}{roscon.Id}/?token={Constants.Token}", content);
 
-            response.EnsureSuccessStatusCode();
+                response.EnsureSuccessStatusCode();
 
-            resp = await response.Content.ReadAsStringAsync();
+                resp = await response.Content.ReadAsStringAsync();
 
-            return JsonConvert.DeserializeObject<Roscon>(resp);
+                return JsonConvert.DeserializeObject<Roscon>(resp);
+            }
+            catch (Exception e)
+            {
+                Log.Warning("UpdateRosconAsync error", $"Error -> {e.Message}");
+                throw e;
+            }
         }
     }
 }
